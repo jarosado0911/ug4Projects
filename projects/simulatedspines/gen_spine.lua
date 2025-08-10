473f9641982378fa551e6af453ff3d3ad06aee26
root <root@DESKTOP-R1UGLGT.localdomain>
2025-08-09 21:56:36 -0400

    updated gen_spine to save .ugx

--- projects/simulatedspines/gen_spine.lua
+++ projects/simulatedspines/gen_spine.lua
@@ -18,14 +18,75 @@ local hlp = require "helperf"
 hlp.initialize_script()
 
 -- choose outfile directory
-out_name = util.GetParam("-outName","output")
-file_exists = hlp.dir_exists(out_name);
+outName = util.GetParam("-outName","output")
+file_exists = hlp.dir_exists(outName);
 if not file_exists then
     print("Output directory exists: "..tostring(file_exists))
-    os.execute("mkdir -p " .. out_name)
-    print("Made directory: "..out_name)
+    os.execute("mkdir -p " .. outName)
+    print("Made directory: "..outName)
 else
-    print("Directory: "..out_name.." exists!")
+    print("Directory: "..outName.." exists!")
 end
 
+-- set the output grid file name
+gridName = outName .. "/artificalspine.ugx"
 
+-- now let us collect some parameters from the user
+-- app length
+neckRad = util.GetParamNumber("-neckRad", 0.08)
+neckLen = util.GetParamNumber("-neckLen", 0.7)
+headRad = util.GetParamNumber("-headRad", 0.29)
+headLen = util.GetParamNumber("-headLen", 0.58)
+appRad = util.GetParamNumber("-appRad", 0.025)
+appLen = util.GetParamNumber("-appLen", 0.5)
+appHeadLen = util.GetParamNumber("-appHeadLen", 0.0)
+appHeadRad = util.GetParamNumber("-appHeadRad", 0.0)
+buildApp = true
+if appLen == 0 then
+	buildApp = false
+end
+buildAppHead = false
+if appHeadLen > 0 and appHeadRad > 0 then
+	buildAppHead = true
+end
+
+-- print a summary of parameters
+print("\n  Summary of Morphology Specifications")
+print("------------------------------------------")
+print("\tneckRad:      "..tostring(neckRad))
+print("\tneckLen:      "..tostring(neckLen))
+print("\theadRad:      "..tostring(headRad))
+print("\theadLen:      "..tostring(headLen))
+print("\tappRad:       "..tostring(appRad))
+print("\tappLen:       "..tostring(appLen))
+print("\tappHeadLen:   "..tostring(appHeadLen))
+print("\tappHeadRad:   "..tostring(appHeadRad))
+print("\tbuildApp:     "..tostring(buildApp))
+print("\tbuildAppHead: "..tostring(buildAppHead))
+
+-- you will need tetgen to run this method
+BuildDendrite(
+	{
+		0.45,	-- cytosol radius (um)
+		0.11,	-- ER radius (um)
+		10.0,	-- dendrite length (um)
+		5.0,	-- spine position (um)
+		appRad,	-- app neck radius (um)
+		appLen,	-- app neck length (um)
+		appHeadRad - appRad,	-- app head radius (in addition to app neck radius) (um)
+		appHeadLen,	-- app head length (um)
+		neckRad,	-- spine neck radius (um)
+		neckLen,-- spine neck length (um)
+		headRad - neckRad,-- spine head radius (in addition to spine neck radius) (um)
+		headLen	-- spine head length (um)
+	},
+	{
+		true,	-- synapse?
+		true,	-- ER?
+		buildApp,	-- app?
+		false,	-- never use that
+		buildAppHead	-- app head?
+	},
+	gridName
+)
+print("Building the dendrite")
